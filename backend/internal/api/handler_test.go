109a28e3e5f7cdaebd01d8e9b9d852e2f62e9c35
szabolcssajben <szabolcs.sajben@outlook.com>
2025-04-13 10:49:03 +0100

    [TESTS] Move test into its own file

--- backend/internal/api/handler_test.go
+++ backend/internal/api/handler_test.go
@@ -1,15 +1,11 @@
 package api
 
 import (
-	"context"
-	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/szabolcssajben/market-tracker/internal/db"
-	"github.com/szabolcssajben/market-tracker/internal/testutils"
 )
 
 func TestHealthHandler(t *testing.T) {
@@ -21,37 +17,4 @@ func TestHealthHandler(t *testing.T) {
 
 	// Assert the status code is 200
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
-}
-
-func TestGetLatestMarket(t *testing.T) {
-	testutils.LoadEnv(t)
-	conn, err := db.ConnectDB()
-	assert.NoError(t, err)
-	defer conn.Close(context.Background())
-
-	req := httptest.NewRequest("GET", "/api/markets/latest", nil)
-	w := httptest.NewRecorder()
-
-	// Call the yet-to-be-implemented handler
-	GetLatestMarketsHandler(conn).ServerHTTP(w, req)
-	resp := w.Result()
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
-
-	var data []db.MarketData
-	err = json.NewDecoder(resp.Body).Decode(&data)
-	assert.NoError(t, err)
-	assert.NotEmpty(t, data)
-
-	// Sanity check
-	for _, row := range data {
-		assert.NotEmpty(t, row.IndexName)
-		assert.NotEmpty(t, row.Region)
-		assert.NotEmpty(t, row.Currency)
-		assert.NotZero(t, row.Timestamp)
-		assert.NotZero(t, row.OpenPrice)
-		assert.NotZero(t, row.ClosePrice)
-		assert.NotZero(t, row.High)
-		assert.NotZero(t, row.Low)
-		assert.NotZero(t, row.Volume)
-	}
 }
